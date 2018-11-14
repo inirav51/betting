e47ba878cbcfb9c99539fc837334ded6d0b98314
Nirav Patel <inirav51@Niravs-MacBook-Pro.local>
2018-11-14 18:41:18 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import betting from './betting';
 
 class App extends Component {
+	state = {
+		dealer: '',
+		
+	};
+	
+	async componentDidMount() {
+		const dealer = await betting.methods.dealer().call();
+		
+		this.setState ({ dealer });
+	}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+		<h2> Betting Contract</h2>
+		<p>This Contract is managed by {this.state.dealer} </p>
+		</div>
     );
   }
 }
